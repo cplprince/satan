aaf6291451dfdb933e9ac898881175a4901af48f
Tomáš Endrych <tomas.endrych@superkoders.com>
2023-02-20 17:38:28 +0100

    numbers

---
+++ index.js
@@ -0,0 +1,21 @@
+let karel = 10.9 + 5.1 + 8.4784;
+const jarmila = 3;
+
+// const result = karel + jarmila;
+
+// +
+// -
+// *
+// /
+// 8**2 // 8 na druhou
+// 10%3 = 1 //celočíselný zbytek po dělení
+
+const grams = 845;
+
+const result = grams / 1000;
+
+console.log(result);
+
+//palce na cm
+//BMI
+//rychlosti
